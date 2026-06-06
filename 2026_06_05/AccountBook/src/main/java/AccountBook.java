3ea9bc87badac768ede734cfa876e4e4e5b1f1f9
“HanChaehui” <hch8344@naver.com>
2026-06-07 05:01:11 +0900

    AccountBook

--- 2026_06_05/AccountBook/src/main/java/AccountBook.java
+++ 2026_06_05/AccountBook/src/main/java/AccountBook.java
@@ -1,2 +1,6 @@
-public class AccountBook {
+public interface AccountBook {
+    void addAccount(); // 1. 내역 추가
+    void showAccount(); // 2. 내역 조회
+    void deleteAll(); // 3. 전체 삭제
+    void deleteItem(); // 4. 내역 삭제
 }
