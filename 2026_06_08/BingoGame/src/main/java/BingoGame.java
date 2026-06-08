aa02416774a00dd84c6f07c44285a1c01cb57f84
“HanChaehui” <hch8344@naver.com>
2026-06-08 17:27:35 +0900

    BingoGame

---
+++ 2026_06_08/BingoGame/src/main/java/BingoGame.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BingoGame {
+
+    static final int SIZE = 5; // 판 크기
+    static final int MAX = 25; // 최대 숫자 범위 제한
+
+    // 1. 시작 화면 띄우기
+    public void play(){
+        System.out.println("===== 빙고 게임 =====");
+        System.out.println("컴퓨터와 번갈아 숫자를 불러 빙고를 완성하세요!");
+    }
+
+    // 2. 빙고판 만들기
+    public void makeBoard(int[][] board){
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < MAX; i++) nums.add(i);
+        Collections.shuffle(nums); // 랜덤으로 섞기
+
+        int idx = 0;
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                board[i][j] = nums.get(idx++); // 1 ~ 25 사이 랜덤 숫자
+            }
+        }
+    }
+
+    // 3. 빙고판 출력
+    public void printBoard(int[][] board, boolean[][] marked){
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                if(marked[i][j]) System.out.print("[ ★] ");
+                else System.out.printf("[%2d] ", board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    // 4. 숫자 마킹
+    public void mark(int[][] board, boolean[][] marked, int num){
+        for(int i = 0; i < SIZE; i++){
+            for(int j = 0; j < SIZE; j++){
+                if(board[i][j] == num) marked[i][j] = true;
+            }
+        }
+    }
+
+    // 5. 빙고 줄 세기
+    public int countBingo(boolean[][] marked){
+        int count = 0;
+
+        // 가로
+        for(int i = 0; i < SIZE; i++){
+            boolean bingo = true;
+            for(int j = 0; j < SIZE; j++){
+                if(!marked[i][j]) bingo = false;
+            }
+            if(bingo) count++;
+        }
+        // 세로
+        for(int i = 0; i < SIZE; i++){
+            boolean bingo = true;
+            for(int j = 0; j < SIZE; j++){
+                if(!marked[j][i]) bingo = false;
+            }
+            if(bingo) count++;
+        }
+        // 대각선 \
+        boolean bingo1 = true;
+        for(int i = 0; i < SIZE; i++){
+            if(!marked[i][i]) bingo1 = false;
+        }
+        if(bingo1) count++;
+
+        // 대각선 /
+        boolean bingo2 = true;
+        for(int i = 0; i < SIZE; i++){
+            if(!marked[i][SIZE - 1 - i]) bingo2 = false;
+        }
+        if(bingo2) count++;
+
+        return count;
+    }
+}
