7bdb484c2fb93ed78485593b845ce9b3b5452082
“HanChaehui” <hch8344@naver.com>
2026-06-12 10:15:34 +0900

    Member-interface

--- 2026_06_11/assignment2/Member-interface/src/main/java/Member.java
+++ 2026_06_11/assignment2/Member-interface/src/main/java/Member.java
@@ -1,2 +1,14 @@
-public class Member {
+public interface Member {
+
+    public String getName();
+    public String getEmail();
+    public String getPhone();
+    public abstract String getGrade();
+    public abstract String getBenefit();
+
+    public void update(String name, String email, String phone);
+
+    default void printInfo(){
+        System.out.println("[" + getGrade() + "] " + getName() + " / " + getEmail() + " / " + getPhone() + " (혜택: " + getBenefit() + ")");
+    }
 }
