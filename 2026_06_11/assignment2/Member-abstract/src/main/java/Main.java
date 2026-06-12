fbec3e42057c5b8155628a8bd7fda50aa815ad2c
“HanChaehui” <hch8344@naver.com>
2026-06-12 09:55:54 +0900

    Member-abstract

--- 2026_06_11/assignment2/Member-abstract/src/main/java/Main.java
+++ 2026_06_11/assignment2/Member-abstract/src/main/java/Main.java
@@ -25,6 +25,9 @@ public class Main {
                     else{
                         System.out.println("등급 [1]일반 [2]VIP");
                         int grade = Integer.parseInt(sc.nextLine());
+                        if(!((grade == 1) || (grade == 2))){
+                            System.out.println("잘못된 입력입니다.");
+                        }
                         System.out.print("이름 > ");   String name  = sc.nextLine();
                         System.out.print("이메일 > "); String email = sc.nextLine();
                         System.out.print("연락처 > "); String phone = sc.nextLine();
