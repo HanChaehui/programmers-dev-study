238722fc6945ced82571a097624dc22f6f87edd8
“HanChaehui” <hch8344@naver.com>
2026-06-15 17:48:49 +0900

    assignment2

--- 2026_06_15/assignment2/HashMap/src/main/java/MyHashMap.java
+++ 2026_06_15/assignment2/HashMap/src/main/java/MyHashMap.java
@@ -2,7 +2,7 @@ public class MyHashMap {
 
     static class Node {
         String key;
-        int value;
+        Integer value;
         Node next;
         Node(String key, int value){
             this.key = key;
@@ -16,6 +16,101 @@ public class MyHashMap {
 
     public MyHashMap() {
         Node[] buckets = new Node[capacity];
+    }
+
+    public int getIndex(String key){
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    public void put(String key, Integer value){
+
+        int index = getIndex(key);
+
+        Node head = buckets[index];
+        while(head != null){
+            if(key.equals(head.key)){
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        Node node = new Node(key, value);
+        node.next = head;
+        buckets[index] = node;
+        size++;
+    }
+
+    public Integer get(String key){
+        int index = getIndex(key);
+
+        Node head = buckets[index];
+        while(head != null){
+            if(key.equals(head.key)){
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean containsKey(String key){
+        int index = getIndex(key);
+
+        Node head = buckets[index];
+        while (head != null) {
+            if (key.equals(head.key)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+//    public Integer remove(String key){
+//        int index = getIndex(key);
+//        Node prev = buckets[index];
+//        if (prev != null && key.equals(prev.key)) {
+//            buckets[index] = prev.next;
+//            size--;
+//            return prev.value;
+//        }
+//        Node node = prev.next;
+//        while (node != null) {
+//            if (key.equals(node.key)) {
+//                prev.next = node.next;
+//                size--;
+//                return node.value;
+//            }
+//            prev = prev.next;
+//            node = node.next;
+//        }
+//        return null;
+//    }
+
+    public Integer remove(String key){
+        int index = getIndex(key);
+        Node node = buckets[index];
+        Node prev = null;
+
+        while (node != null) {
+            if (key.equals(node.key)) {
+                if(prev == null){
+                    buckets[index] = node.next;
+                    size--;
+                    return node.value;
+                }
+                prev.next = node.next;
+                size--;
+                return node.value;
+            }
+            prev = prev.next;
+            node = node.next;
+        }
+        return null;
     }
 
 }
