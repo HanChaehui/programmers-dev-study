a622d1491bef18837b83f2f20d72f1262b59ac6f
“HanChaehui” <hch8344@naver.com>
2026-06-29 03:58:26 +0900

    Stream

---
+++ 2026_06_24/Java-Stream/src/main/java/Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    private int id;
+    private java.util.List<String> items;
+
+    Order(int id, java.util.List<String> items){
+        this.id = id;
+        this.items = items;
+    }
+
+    public java.util.List<String> getItems() { return items; }
+}
---
+++ 2026_06_24/Java-Stream/src/main/java/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    private String name;
+    private int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+    public int getPrice() { return price; }
+}
