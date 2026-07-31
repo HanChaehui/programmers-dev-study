cf561ec18dd7b691e2e9df3802f769150bea0087
“HanChaehui” <hch8344@naver.com>
2026-07-31 16:47:21 +0900

    token

---
+++ 2026_07_31/Board-Token/src/main/java/com/example/spring/boardtoken/util/SecretKeyGen.java
@@ -0,0 +1,20 @@
+package com.example.spring.security3token.util;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+// 테스트코드 쪽으로 빼기
+
+public class SecretKeyGen {
+
+    private static final int KEY_LENGTH_BYTES = 64;
+
+    static void main(String[] args) {
+        byte[] bytes = new byte[KEY_LENGTH_BYTES];
+
+        new SecureRandom().nextBytes(bytes);
+
+        System.out.println( Base64.getEncoder().encodeToString(bytes) );
+    }
+
+}
